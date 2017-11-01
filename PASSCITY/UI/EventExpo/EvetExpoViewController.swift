62a062578e1262a4dff7ee007aba000cc07e1477
blkshdw <aleksey.glebow@gmail.com>
2017-11-01 14:01:29 +0300

    Configure expo event screen

---
+++ PASSCITY/UI/EventExpo/EvetExpoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EvetExpoViewController.swift
+//  PASSCITY
+//
+//  Created by Алексей on 02.11.17.
+//  Copyright © 2017 PASSCITY-iOS-TEST. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import EasyPeasy
+
+class EventExpoViewController: UITableViewController {
+  
+}
